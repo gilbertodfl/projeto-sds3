6b49375cab9688557f221692dffba8113ac86bac
Gilberto de Freitas Lins <gilbertodefreitaslins@hotmail.com>
2021-09-10 23:09:42 -0300

    Group by search

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSucessDTO.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SaleSucessDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String sellerName;
+	private Long visited;
+	private Long deals;
+	
+	public SaleSucessDTO() {
+		
+	}
+/*
+ * Aqui trocamos String sellerName por Seller seller
+ * public SaleSucessDTO(String sellerName, Long visited, Long deals) 
+ * 
+ * Também trocamos sellerName = seller.getName();;
+ */
+	
+		
+	public SaleSucessDTO(Seller seller, Long visited, Long deals) {
+		sellerName = seller.getName();
+		this.visited = visited;
+		this.deals = deals;
+	}
+
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+
+	public Long getVisited() {
+		return visited;
+	}
+
+
+	public void setVisited(Long visited) {
+		this.visited = visited;
+	}
+
+
+	public Long getDeals() {
+		return deals;
+	}
+
+
+	public void setDeals(Long deals) {
+		this.deals = deals;
+	}
+	
+	
+}
