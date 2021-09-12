040b07eba53338fd772e690c267d3a7dd971c778
gilbertodfl <84579809+gilbertodfl@users.noreply.github.com>
2021-09-12 15:16:01 -0300

    Update index.tsx
    remove line 4: import { isTemplateExpression } from "typescript";

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
-import { isTemplateExpression } from "typescript";
+
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from 'utils/requests';
 import  Pagination  from 'components/Pagination';
